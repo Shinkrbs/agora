55551c7ab712014c72f65eb7fcbb6fc416e47285
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2026-03-19 17:47:25 +0800

    feat: created signup zod schema for form validation

--- lib/schema.ts
+++ lib/schema.ts
@@ -2,7 +2,24 @@ import * as z from "zod";
 
 export const loginSchema = z.object({
   email: z.email("Invalid email address."),
-  password: z.string().min(8, "Password must be atleast 8 characters long."),
+  password: z.string(),
 });
 
+export const signUpSchema = z
+  .object({
+    first_name: z.string().min(1, "This field must not be empty."),
+    middle_name: z.string(),
+    last_name: z.string().min(1, "This field must not be empty."),
+    suffix: z.string(),
+    username: z.string().min(8, "Usernames must be atleast 8 characters long."),
+    email: z.string().email("Invalid email address."),
+    password: z.string().min(8, "Passwords must be atleast 8 characters long."),
+    confirm_password: z.string(),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: "Passwords do not match",
+    path: ["confirm_password"],
+  });
+
 export type LoginSchema = z.infer<typeof loginSchema>;
+export type SignUpSchema = z.infer<typeof signUpSchema>;

d7bffbf9dde9a3165cb5e0884fc2450044303061
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2026-03-19 22:01:23 +0800

    feat: created update password page

---
+++ app/(public)/(auth)/update-password/page.tsx
@@ -0,0 +1,13 @@
+import { UpdatePasswordForm } from "./_components/UpdatePasswordForm";
+
+const page = () => {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-4 md:p-6">
+      <div className="w-full max-w-md">
+        <UpdatePasswordForm />
+      </div>
+    </div>
+  );
+};
+
+export default page;

97d276754c2fbe39ac69d963722745ceabd6ab4c
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2026-03-19 11:53:50 +0800

    feat: added simpe get user check to see if sign in worked

--- app/authenticated/dashboard/page.tsx
+++ app/authenticated/dashboard/page.tsx
@@ -1,3 +1,15 @@
-export default function Dashboard() {
-  return <div>This is dashboard.</div>;
+import { createClient } from "@/lib/supabase/server";
+import { cookies } from "next/headers";
+
+export default async function Dashboard() {
+  const supabase = await createClient(await cookies());
+  const { data } = await supabase.auth.getUser();
+
+  const { data: userData } = await supabase
+    .from("users")
+    .select("first_name")
+    .eq("id", data.user?.id)
+    .single();
+
+  return <div>Hello {userData?.first_name}! This is dashboard.</div>;
 }

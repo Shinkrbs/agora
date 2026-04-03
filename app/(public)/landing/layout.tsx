2693a98f075099b873d0303f5d242be53d2b227a
Shn Kerby Dolamos <23-1-02186@vsu.edu.ph>
2026-04-03 19:25:01 +0800

    Fix: Rename import for themes

--- app/(public)/landing/layout.tsx
+++ app/(public)/landing/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Arimo, Space_Mono } from "next/font/google";
 // 1. Import the Theme Provider you created
-import { ThemeProvider } from "@/components/ThemeProvider";
+import { ThemeProvider } from "@/components/themeprovider";
 import { LandingPageHeader } from "./_components/LandingPageHeader";
 import LandingPageFooter from "./_components/LandingPageFooter";
 

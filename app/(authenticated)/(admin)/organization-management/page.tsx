98663ceb1312f4239e4e690dea620786600f5c0a
jamir4270 <jamirandrade4270@gmail.com>
2026-04-04 21:52:25 +0800

    feat: combined all organization management components in main page

--- app/(authenticated)/(admin)/organization-management/page.tsx
+++ app/(authenticated)/(admin)/organization-management/page.tsx
@@ -1,3 +1,17 @@
-export default function page() {
-  return <div>This is organization management page.</div>;
+import {
+  OrganizationsHeader,
+  OrganizationsGrid,
+} from "./_components";
+import { getUserOrganizations } from "./_queries/organization-management-queries";
+
+export default async function OrganizationManagementPage() {
+  const organizations = await getUserOrganizations();
+  return (
+    <div className="min-h-screen p-8">
+      <div className="max-w-7xl mx-auto">
+        <OrganizationsHeader />
+        <OrganizationsGrid organizations={organizations.organizations} />
+      </div>
+    </div>
+  );
 }

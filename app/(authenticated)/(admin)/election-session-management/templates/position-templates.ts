e828fe3553da8f81fe02d89ae12e03c4b6e51faf
jamir4270 <jamirandrade4270@gmail.com>
2026-04-06 22:28:58 +0800

    feat: created position template

---
+++ app/(authenticated)/(admin)/election-session-management/templates/position-templates.ts
@@ -0,0 +1,12 @@
+export const DEFAULT_POSITIONS = [
+  { name: "President", seat_count: 1 },
+  { name: "Vice President", seat_count: 1 },
+  { name: "Secretary", seat_count: 1 },
+  { name: "Treasurer", seat_count: 1 },
+  { name: "Auditor", seat_count: 1 },
+  { name: "Public Information Officer", seat_count: 1 },
+  { name: "First Year Representative", seat_count: 2 },
+  { name: "Second Year Representative", seat_count: 2 },
+  { name: "Third Year Representative", seat_count: 2 },
+  { name: "Fourth Year Representative", seat_count: 2 },
+];

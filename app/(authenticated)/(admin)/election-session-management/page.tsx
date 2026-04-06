2a9ee42e4ea40b52eddd4a4af9488c4a4b1e544b
jamir4270 <jamirandrade4270@gmail.com>
2026-04-06 16:10:43 +0800

    feat: created initial design and layout of election session management page

--- app/(authenticated)/(admin)/election-session-management/page.tsx
+++ app/(authenticated)/(admin)/election-session-management/page.tsx
@@ -1,3 +1,10 @@
-export default function page() {
-  return <div>This is election session page.</div>;
+import { ElectionSessionsList } from "./_components";
+import { dummyElections } from "./_data/dummy-elections";
+
+export default function ElectionSessionManagementPage() {
+  return (
+    <div className="space-y-6">
+      <ElectionSessionsList initialElections={dummyElections} />
+    </div>
+  );
 }

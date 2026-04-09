4256d431aebcf4fe5c5bc39c5615701d3a81e423
jamir4270 <jamirandrade4270@gmail.com>
2026-04-09 14:11:37 +0800

    feat: added new position template page in admin sidebar

---
+++ app/(authenticated)/(admin)/position-templates/page.tsx
@@ -0,0 +1,8 @@
+export default function PositionTemplatesPage() {
+  return (
+    <div>
+      <h1>Position Templates</h1>
+      {/* Content for position templates goes here */}
+    </div>
+  );
+}

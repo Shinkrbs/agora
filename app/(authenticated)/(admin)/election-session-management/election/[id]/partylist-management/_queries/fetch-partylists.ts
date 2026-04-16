46518b6cc6332b10766accb0926c26df00645950
jamir4270 <jamirandrade4270@gmail.com>
2026-04-16 13:33:50 +0800

    fix: fixed supabase query in getting candidate count

--- app/(authenticated)/(admin)/election-session-management/election/[id]/partylist-management/_queries/fetch-partylists.ts
+++ app/(authenticated)/(admin)/election-session-management/election/[id]/partylist-management/_queries/fetch-partylists.ts
@@ -8,13 +8,12 @@ export async function fetchPartylists(electionId: string): Promise<{data: Partyl
         const supabase = await createClient(cookieStore);
         
         const { data, error } = await supabase.from("partylists").select("*, candidates(count)").eq("election_id", electionId).eq("is_deleted", false);
-
         if (error) {
             throw new Error("Failed to fetch partylists");
         }
 
         const formattedData: PartylistWithCandidateCount[] = (data || []).map((partylist) => {
-            const count = partylist.candidates?.count || 0;
+            const count = partylist.candidates?.[0]?.count || 0;
             const { candidates, ...rest } = partylist;
 
             return {
@@ -22,10 +21,8 @@ export async function fetchPartylists(electionId: string): Promise<{data: Partyl
                 candidate_count: count,
             }
         })
-        console.log("Fetched partylists with candidate counts:", formattedData);
         return { data: formattedData, error: null };
     } catch (error) {
-        console.error("Error fetching partylists:", error);
         return { data: null, error: "Failed to fetch partylists" };
     }
 }

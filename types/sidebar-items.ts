f2eb2a30716832448b6ef221fe7f89b505ca09b6
jamir4270 <jamirandrade4270@gmail.com>
2026-06-01 01:30:31 +0800

    chore: removed unused imports and rewrote outdated styling

--- types/sidebar-items.ts
+++ types/sidebar-items.ts
@@ -1,4 +1,4 @@
-import { LayoutDashboard, UserCog, FileUser, Vote, Building2, Settings, type LucideIcon, ClipboardList} from "lucide-react";
+import { LayoutDashboard, UserCog, Vote, Building2, Settings, type LucideIcon, ClipboardList} from "lucide-react";
 
 export type SidebarItem = {
   title: string;

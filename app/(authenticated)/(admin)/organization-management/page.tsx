3d56fe82c76cffa77848837e99be28329c3799ac
jamir4270 <jamirandrade4270@gmail.com>
2026-06-07 19:34:05 +0800

    refactor: utilize organization container component for better ui handling in main page

--- app/(authenticated)/(admin)/organization-management/page.tsx
+++ app/(authenticated)/(admin)/organization-management/page.tsx
@@ -1,17 +1,11 @@
 import {
-  OrganizationsHeader,
-  OrganizationsGrid,
+  OrganizationsContainer,
 } from "./_components";
 import { getUserOrganizations } from "./_queries/organization-management-queries";
 
 export default async function OrganizationManagementPage() {
   const organizations = await getUserOrganizations();
   return (
-    <div className="min-h-screen p-5 py-1">
-      <div className="max-w-7xl mx-auto">
-        <OrganizationsHeader />
-        <OrganizationsGrid organizations={organizations.organizations} />
-      </div>
-    </div>
+    <OrganizationsContainer organizations={organizations.organizations} />
   );
 }
